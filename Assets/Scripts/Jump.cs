232f792fddc81f97be43b7806bd4522cc821779a
nethos <geeklego@gmail.com>
2022-09-27 16:04:58 +0200

    jump variables creation, player scale returned to 1

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+    [SerializeField] private int maxJumps = 2;
+    [SerializeField] private float minJumpHeight = 2.5f;
+    [SerializeField] private float maxJumpTime = 0.5f;
+    [SerializeField] private float gravity = 9f;
+
+    private float speed = 0;
+
+    private int jumpsLeft;
+    private bool isJumping = false;
+
+    public bool OnGround { get; private set; } = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        jumpsLeft = maxJumps;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position += Vector3.zero;
     }
 }
