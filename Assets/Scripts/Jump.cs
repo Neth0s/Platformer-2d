987e6f099abb58bcf3f40991790343256852b434
nethos <geeklego@gmail.com>
2022-10-04 15:27:45 +0200

    added variable jump height, fall multiplier and jump cutoff

--- Assets/Scripts/Jump.cs
+++ Assets/Scripts/Jump.cs
@@ -5,17 +5,24 @@ using UnityEngine;
 public class Jump : MonoBehaviour
 {
     [Header("Jump parameters")]
-    [SerializeField] private int maxJumps = 2;
-    [SerializeField] private float jumpImpulse = 2.5f;
+    [SerializeField, Range(0, 10)] private int maxJumps = 2;
+    [SerializeField, Range(0, 50)] private float jumpImpulse = 10f;
 
     [Header("Falling")]
     [SerializeField] private float gravity = 10f;
+
+    [Tooltip("Multiplier applied to gravity when falling.")]
+    [SerializeField, Range(1, 10)] private float fallMultiplier = 1.5f;
+
+    [Tooltip("Percentage of vertical speed removed if jump button is released before end of jump.")]
+    [SerializeField, Range(0, 100)] private int jumpCutoff = 50;
 
     private Manette inputActions;
     private float speed = 0;
 
     private int jumpsLeft;
     private bool isJumping = false;
+    private bool cutoffApplied = false;
 
     public bool OnGround { get; private set; } = false;
     public float VerticalSpeed { get { return speed; } }
@@ -33,26 +40,25 @@ public class Jump : MonoBehaviour
     void FixedUpdate()
     {
         GetInput();
-        speed -= gravity * Time.deltaTime;
+        speed -= (speed < 0 ? fallMultiplier : 1) * gravity * Time.deltaTime;
         transform.position += speed * Time.deltaTime * Vector3.up;
     }
 
     private void GetInput()
     {
         float input = inputActions.Player.Jump.ReadValue<float>();
-        if (input != 0)
+        if (input != 0 && jumpsLeft > 0)
         {
-            if (isJumping)
-            {
-                //TODO
-            }
-            else if (jumpsLeft > 0)
-            {
-                jumpsLeft--;
-                OnGround = false;
-                isJumping = true;
-                speed = jumpImpulse;
-            }
+            jumpsLeft--;
+            OnGround = false;
+            isJumping = true;
+            speed = jumpImpulse;
+        }
+
+        if (isJumping && input == 0 && speed > 0 && !cutoffApplied)
+        {
+            speed *= (100f - jumpCutoff) / 100f;
+            cutoffApplied = true;
         }
     }
 
@@ -61,6 +67,7 @@ public class Jump : MonoBehaviour
         StopSpeed();
         OnGround = true;
         isJumping = false;
+        cutoffApplied = false;
         jumpsLeft = maxJumps;
     }
 
